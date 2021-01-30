11010a6855538363341e4b4b5a36d984dae3707d
Filippo Massarelli <filippomassarelli@Filippos-MacBook-Pro-2.local>
2021-01-30 10:54:00 +0300

    change favicon to saucer. Change banner to use black gif with Seedrs Theme. Add grid structure to banner for good looking responsive layout

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import { useTheme } from "@material-ui/styles";
 import React from "react";
 import { useEffect } from "react";
@@ -9,6 +9,7 @@ import requests from "../../requests";
 
 function Banner() {
   const theme = useTheme();
+  console.log(theme.palette.primary.main);
   const [campaign, setCampaign] = useState([]);
   const synonymOfChanging = [
     "changing",
@@ -33,55 +34,78 @@ function Banner() {
     fetchData();
   }, []);
   return (
-    <div
-      style={{
-        backgroundSize: "contain",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "95% 50%",
-        backgroundColor: theme.palette.primary.light,
-        backgroundImage: `url(https://media.giphy.com/media/L3EZ0FdfMbjgNEpNLC/giphy.gif)`,
-        height: "50vh",
-        minHeight: "200px",
-      }}
-    >
-      <div
-        style={{
-          height: "100%",
-          display: "flex",
-          maxWidth: "50%",
-          flexDirection: "column",
-          alignContent: "flex-end",
-          justifyContent: "center",
-          marginLeft: "10%",
-        }}
-      >
-        <Typography variant="h5" color="textSecondary" gutterBottom>
-          Hey explorer!
-        </Typography>
-        <Typography variant="body1" color="textSecondary">
-          {campaign.name} is{" "}
-          <strong>
-            {
-              synonymOfChanging[
-                Math.floor(Math.random() * synonymOfChanging.length)
-              ]
-            }
-          </strong>{" "}
-          the {campaign.sector} industry in <strong>{campaign.country}</strong>.
-        </Typography>
-
-        <Link
-          to={{ pathname: `/:${campaign.id}` }}
-          style={{ textDecoration: "none", marginTop: "20px" }}
-        >
-          <Button variant="contained" color="primary">
-            <Typography variant="button" color="textSecondary">
-              Check it out
+    <Grid container direction="column">
+      <Grid item container>
+        <Grid item xs={false} md={1} lg={2} />
+        <Grid item xs={10} md={7} lg={7}>
+          <div
+            style={{
+              height: "100%",
+              display: "flex",
+              maxWidth: "50%",
+              flexDirection: "column",
+              justifyContent: "center",
+              margin: "15px 0 50px 35px",
+            }}
+          >
+            <Typography variant="h5" color="inherit" gutterBottom>
+              Hey explorer!
             </Typography>
-          </Button>
-        </Link>
-      </div>
-    </div>
+            <Typography variant="body1" color="inherit">
+              {campaign.name} is{" "}
+              <strong>
+                {
+                  synonymOfChanging[
+                    Math.floor(Math.random() * synonymOfChanging.length)
+                  ]
+                }
+              </strong>{" "}
+              the {campaign.sector} industry in{" "}
+              <strong>{campaign.country}</strong>.
+            </Typography>
+
+            <Link
+              to={{ pathname: `/:${campaign.id}` }}
+              style={{ textDecoration: "none", marginTop: "20px" }}
+            >
+              <Button variant="contained" color="primary">
+                <Typography variant="button" color="inherit">
+                  Check it out
+                </Typography>
+              </Button>
+            </Link>
+          </div>
+        </Grid>
+        <Grid item xs={2} md={3} lg={2}>
+          <div
+            style={
+              theme.palette.primary.main === "#7FBB30"
+                ? {
+                    backgroundSize: "contain",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "0 50%",
+                    marginLeft: "-150px",
+                    backgroundColor: "inherit",
+                    backgroundImage: `url(https://media.giphy.com/media/KApag9isJD1l7y6jmW/giphy.gif)`,
+                    height: "45vw",
+                    maxHeight: "300px",
+                  }
+                : {
+                    backgroundSize: "contain",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "0 50%",
+                    marginLeft: "-150px",
+                    backgroundColor: "inherit",
+                    backgroundImage: `url(https://media.giphy.com/media/L3EZ0FdfMbjgNEpNLC/giphy.gif)`,
+                    height: "45vw",
+                    maxHeight: "300px",
+                  }
+            }
+          ></div>
+        </Grid>
+        <Grid item xs={false} md={1} lg={2} />
+      </Grid>
+    </Grid>
   );
 }
 
